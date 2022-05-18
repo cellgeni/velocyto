59b67cf8396e7ed2b1c0ba313b6737a7cf6ccbb6
Simon Murray <simondonaldmurray@gmail.com>
2022-05-18 16:17:16 +0100

    updated velocyto script

--- scripts/velocyto.sh
+++ scripts/velocyto.sh
@@ -3,14 +3,15 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-im=/nfs/cellgeni/singularity/images/velocyto2.sif
+im=/nfs/cellgeni/singularity/images/velocyto2.sif #velocyto image
 
-TAG=$1
-GTF=/nfs/cellgeni/STAR/human/2020A/GRCh38_v32_filtered.gtf
-RMSK=/nfs/cellgeni/velocyto/GRCh38_rmsk.gtf
+TAG=$1 #sampleID
+GTF=/nfs/cellgeni/STAR/human/2020A/GRCh38_v32_filtered.gtf #gtf file to use
+RMSK=/nfs/cellgeni/velocyto/GRCh38_rmsk.gtf #mask file to use
+MEM=7000 #GB of memory to use
+THREADS=16 #number of threads to use
 
-#velocyto run10x -m $RMSK -t uint32 --samtools-threads 16 --samtools-memory 8000 $DIR $GTF
-#velocyto run10x -t uint32 --samtools-threads 16 --samtools-memory 8000 $DIR $GTF
+###################### DONT CHANGE OPTIONS BELOW THIS LINE ###########################
 
 singularity exec -B /lustre -B /nfs/ $im \
-  velocyto run -t uint32 --samtools-threads 16 --samtools-memory 7000 -b $TAG.barcodes.tsv -o $TAG.velocyto -m $RMSK $TAG.bam $GTF
+  velocyto run -t uint32 --samtools-threads $THREADS --samtools-memory $MEM -b $TAG.barcodes.tsv -o $TAG.velocyto -m $RMSK $TAG.bam $GTF
