5bb3d847f4be38baba169c584837c703e2cc7c62
Simon Murray <simondonaldmurray@gmail.com>
2022-05-18 15:45:51 +0100

    updated getting script

--- scripts/iget_velocyto_bams.sh
+++ scripts/iget_velocyto_bams.sh
@@ -1,23 +1,25 @@
-#!/bin/bash 
+#!/bin/bash
 
-KK=`cut -f1 ../actions/samples.tsv`
-PP=`cut -f2 ../actions/samples.tsv`
+set -euo pipefail
 
-a=( $KK ) 
-b=( $PP ) 
+#run this from data directory
 
-N=`cat ../actions/samples.tsv | wc -l`
+sf=../example-data/irods.txt #tab separated file containing sampleIDs and corresponding irods path to directory
+barcode_path=filtered_feature_bc_matrix/barcodes.tsv.gz #path to filtered barcodes file within irods directory
+bam_file=gex_possorted_bam.bam #name of bam file within irods directory
+index_file=gex_possorted_bam.bam.bai #name of index file within irods directory
 
-for i in `seq 0 $((N-1))`
-do
-  echo "Downloading files for sample ${b[$i]}, directory ${a[$i]}.."
-  iget ${a[$i]}/gex_possorted_bam.bam
-  mv gex_possorted_bam.bam ${b[$i]}.bam 
-  iget ${a[$i]}/gex_possorted_bam.bam.bai
-  mv gex_possorted_bam.bam.bai ${b[$i]}.bam.bai
-  iget ${a[$i]}/filtered_feature_bc_matrix/barcodes.tsv.gz
-  gzip -d barcodes.tsv.gz
-  mv barcodes.tsv ${b[$i]}.barcodes.tsv
-done 
+###################### DONT CHANGE OPTIONS BELOW THIS LINE ###########################
 
-echo "ALL DONE!" 
+cat $sf | head -n 1 | while read name path; do
+  ( echo "-- $path"
+    iget -f -v -N 4 -K $path/$barcode_path
+    gunzip barcodes.tsv.gz
+    mv barcodes.tsv $name.barcodes.tsv
+    echo "✓"
+    iget -f -v -N 4 -K $path/$bam_file
+    mv $bam_file $name.bam
+    iget -f -v -N 4 -K $path/$index_file
+    mv $index_file $name.bam.bai
+  )
+done
